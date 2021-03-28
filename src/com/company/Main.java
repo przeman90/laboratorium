9b23adee21e35d0436134b5991c3df2b25f85ac9
mikus <przeman90@gmail.com>
2021-03-28 11:59:15 +0200

    dorzucone rzeczy do drugiej galezi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,5 +12,8 @@ public class Main {
         System.out.println("odartura1");
         System.out.println("odartura2");
         System.out.println("odartura3");
+        System.out.println("dorzucielem cos jeszcze");
+        System.out.println("dorzucielem cos jeszcze");
+        System.out.println("dorzucielem cos jeszcze");
     }
 }
