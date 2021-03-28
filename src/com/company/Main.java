c47e7dfdb68e0987b8af3264ce626be0bb2b2188
Arturgit9 <artur_kryn@wp.pl>
2021-03-28 12:05:40 +0200

    POPRAWIONE

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,8 +10,12 @@ public class Main {
         System.out.println("odartura");
 
         System.out.println("odartura1");
-        System.out.println("odartura2");
+        System.out.println("!!!!!!!!!!!!!!!!!a TU BYL BLAD!!!!!!!!!!");
         System.out.println("odartura3");
         System.out.println("dorzuciłłem do maina rzeczy od Artura");
+        System.out.println("nowa galaz A2asdasdasdadwrewerererwsa");
+        System.out.println("dnowa galaz A2asdasdasdadsewrwerwerrwaa");
+        System.out.println("dorzucinowa galaz A2asdasdasdadsay od Aewrwerwerrtura");
+        System.out.println("dorzuciłłem nowa galaz A2asdasdasdadsasdfsefewfwewefrtura");
     }
 }
