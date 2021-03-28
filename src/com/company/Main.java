8742b5a3117ff26af8d13656591d5e6e5c02705f
mikus <przeman90@gmail.com>
2021-03-28 10:37:42 +0200

    raz

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Pierwsza linia");
+        System.out.println("druga");
+        System.out.println("i ostatnia");
+    }
+}
